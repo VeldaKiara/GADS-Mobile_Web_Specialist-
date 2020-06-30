69d6c7167062f65b58594aed010e1c0a55503e5b
Velda Kiara <veldakarimi@gmail.com>
2020-06-30 11:51:17 +0300

    equality operators

---
+++ JS/equality.js
@@ -0,0 +1,11 @@
+console.log(1 ==1);
+//true 
+console.log(1 === true)
+//false
+let id = "123"
+console.log(id == 123)
+//true
+console.log(id === 123)
+//true
+ // == attempts to type convert before comparison
+ // === does not type convert.
