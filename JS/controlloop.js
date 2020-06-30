49d706382bcb160293a309a579692d237ce8d49d
Velda Kiara <veldakarimi@gmail.com>
2020-06-30 11:41:13 +0300

    controlling loops

---
+++ JS/controlloop.js
@@ -0,0 +1,12 @@
+for (let i=0; i<5; i++){
+    console.log(i);
+    if (i === 3)
+      break;
+  }
+  
+  //using continue...
+  for (let i=0; i<5; i++){
+    console.log(i);
+    if (i === 3)
+      continue;
+  }
