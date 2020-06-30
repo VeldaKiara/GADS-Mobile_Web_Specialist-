f27ad5c2af43d16647a4ae36fd5e0729cc582f26
Velda Kiara <veldakarimi@gmail.com>
2020-06-30 12:20:26 +0300

    unary operators

---
+++ JS/unary.js
@@ -0,0 +1,11 @@
+let year = 1956
+console.log(year++)
+//value 1956
+console.log(year)
+//value 1957
+
+let y = 1956
+console.log(++y)
+//1957
+//  var++ uses the year first and then increments it after
+//++var increments it and displays .
